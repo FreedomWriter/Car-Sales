69faa8fb2c3448a44ccbb24b4920339fa6d99b6c
FreedomWriter <natalienetworks@gmail.com>
2020-04-14 15:22:15 -0500

    dependency update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,14 +7,14 @@ export const initialState = {
     name: "2019 Ford Mustang",
     image:
       "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-    features: []
+    features: [],
   },
   additionalFeatures: [
     { id: 1, name: "V-6 engine", price: 1500 },
     { id: 2, name: "Racing detail package", price: 1500 },
     { id: 3, name: "Premium sound system", price: 500 },
-    { id: 4, name: "Rear spoiler", price: 250 }
-  ]
+    { id: 4, name: "Rear spoiler", price: 250 },
+  ],
 };
 
 export const reducer = (state = initialState, action) => {
@@ -30,10 +30,10 @@ export const reducer = (state = initialState, action) => {
         car: {
           ...state.car,
           features: [
-            ...state.car.features.filter(item => item !== action.payload)
+            ...state.car.features.filter((item) => item !== action.payload),
           ],
-          price: state.car.price - action.payload.price
-        }
+          price: state.car.price - action.payload.price,
+        },
       };
     case ADD_ITEM:
       console.log(
@@ -46,8 +46,8 @@ export const reducer = (state = initialState, action) => {
         car: {
           ...state.car,
           features: [...state.car.features, action.payload],
-          price: state.car.price + action.payload.price
-        }
+          price: state.car.price + action.payload.price,
+        },
       };
     default:
       return state;
